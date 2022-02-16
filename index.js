f445f539f39ee516d0e6fc188ed3e2387b083da6
Ted Piotrowski <tppiotrowski@gmail.com>
2022-02-16 12:11:25 -0800

    Remove UTC Grid and update deps

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
 var tilelivestream = require('tilelivestreams');
 var fs = require('fs');
-var through = require('through2');
-var mbtiles = require('mbtiles');
+var mbtiles = require('@mapbox/mbtiles');
 var mkdirp = require('mkdirp');
+var through = require('through2');
 
-module.exports = function(inTile, outPath, mbutilMode, callback) {
+var numCompleted = 0;
+module.exports = function(inTile, outPath, callback) {
   if (outPath.slice(-1) !== '/') {
     outPath = outPath + '/';
   }
-  var infoName = typeof mbutilMode !== 'undefined' ? 'metadata.json' : "info.json";
+  var infoName = "info.json";
   new mbtiles(inTile, function(err, tiles) {
-    if (err) {
-      return callback(err);
-    }
-    var tileStream = tilelivestream(tiles);
-    mkdirp(outPath, function (err) {
       if (err) {
-        return callback(err);
+      return callback(err);
       }
-      var writing = tileStream.pipe(through.obj(function (data, _, cb) {
-        var path, outData;
-        if (data.tile || data.grid) {
-          path = outPath + data.z + '/' + data.x + '/';
-          mkdirp(path, function (err) {
-            if (err) {
-              return cb(err);
-            }
-            var fullpath, outdata, options = {};
-            if (data.tile) {
-              fullpath = path + data.y + '.png';
-              outdata = data.tile;
-            } else if (data.grid) {
-              fullpath = path + data.y + ".grid.json";
-               if (mbutilMode) {
-                outdata = 'typeof '+ mbutilMode + " === 'function' && " + mbutilMode + '(' + JSON.stringify(data.grid) + ');';
-              } else {
-                outdata = JSON.stringify(data.grid);
-              }
-              options.encoding = 'utf8';
-            }
-            fs.writeFile(fullpath, outdata, options, cb);
-          });
-        } else if (data.name) {
-          fs.writeFile(outPath + infoName, JSON.stringify(data), {encoding: 'utf8'}, cb);
-        }
-      }));
-      writing.on('error', callback);
-      writing.on('end', function() {
-        callback();
+      var tileStream = tilelivestream(tiles);
+      mkdirp(outPath, function (err) {
+	  if (err) {
+	  return callback(err);
+	  }
+	  var writing = tileStream.pipe(through.obj(function (data, _, cb) {
+		var path, outData;
+		if (data.tile) {
+		path = outPath + data.z + '/' + data.x + '/';
+		mkdirp(path, function (err) {
+		    if (err) {
+		    return cb(err);
+		    }
+		    var fullpath, outdata, options = {};
+		    if (data.tile) {
+		    fullpath = path + data.y + '.pbf';
+		    outdata = data.tile;
+		    }		    
+		    fs.writeFile(fullpath, outdata, options, cb);
+		    numCompleted++;
+		    numCompleted % 1000 === 0 && console.log(numCompleted);
+		    });
+		} else if (data.name) {
+		fs.writeFile(outPath + infoName, JSON.stringify(data), {encoding: 'utf8'}, cb);
+		}
+		}));
+	  writing.on('error', callback);
+	  writing.on('end', function() {
+	      callback();
+	      });
       });
-    });
   });
 };
